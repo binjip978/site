193d665d8d2fe81715c6f747c8aeec2feda4d06c
Siarhiej Siemianczuk <pdp.eleven11@gmail.com>
2024-01-14 10:27:11 +0200

    localmode and even a silly post
    Signed-off-by: Siarhiej Siemianczuk <pdp.eleven11@gmail.com>

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"embed"
+	"flag"
 	"io/fs"
 	"log"
 	"net/http"
@@ -13,11 +14,17 @@ import (
 var static embed.FS
 
 func main() {
+	var local = flag.Bool("local", false, "run locally")
+	flag.Parse()
 	mux := http.NewServeMux()
 	static, err := fs.Sub(static, "static")
 	if err != nil {
 		log.Fatal(err)
 	}
 	mux.Handle("/", http.FileServer(http.FS(static)))
+	if *local {
+		log.Println("running locally")
+		log.Fatal(http.ListenAndServe(":8080", mux))
+	}
 	log.Fatal(http.Serve(autocert.NewListener("binjip978.net"), mux))
 }
